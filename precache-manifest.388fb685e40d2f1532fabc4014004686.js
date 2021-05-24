3af6da850e5edc2c336c7162ddb1d31fd5f6e9f1
Maestro263 <38524855+Maestro263@users.noreply.github.com>
2021-05-24 21:38:46 +0200

    Add files via upload

---
+++ precache-manifest.388fb685e40d2f1532fabc4014004686.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f33dfd21a928a49464246be8958fb03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfdb95de8885ec8013b5",
+    "url": "/static/js/2.da91ecb4.chunk.js"
+  },
+  {
+    "revision": "ced9c5621a5b67891bd6",
+    "url": "/static/js/main.ec436281.chunk.js"
+  },
+  {
+    "revision": "de156563810503183e0a",
+    "url": "/static/js/runtime-main.e9491d4c.js"
+  }
+]);
